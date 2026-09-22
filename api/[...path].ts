fb101b3970d0378e42fcabc3e2ca58f710992ad3
hamsathgamedesign-svg <hamsathgamedesign@gmail.com>
2026-09-22 23:27:30 +0530

    Delete api directory

--- api/[...path].ts
+++
@@ -1,7 +0,0 @@
-import { handleDayPilotApi } from '../lib/daypilot-api'
-
-export const config = { runtime: 'nodejs' }
-
-export default function handler(req: any, res: any) {
-  return handleDayPilotApi(req, res)
-}
--- api/account/signup.ts
+++
@@ -1,5 +0,0 @@
-export const config = { runtime: 'nodejs' }
-import { handleDayPilotApi } from '../../lib/daypilot-api'
-export default function handler(req: any, res: any) {
-  return handleDayPilotApi(req, res, '/api/account/signup')
-}
